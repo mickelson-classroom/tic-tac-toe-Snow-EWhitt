f10dc659aa3f9b07f6bd634157728a1688cbe03a
Snow-EWhitt <admin_ethan@EWL>
2023-08-26 11:54:01 -0600

    showing the past moves

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -4,10 +4,29 @@ export default function Game() {
   const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const currentSquares = history[history.length - 1];
+  const moves = history.map((squares, move) => {
+    let description;
+
+    if (move > 0) {
+      description = 'Go to move #' + move;
+    } else {
+      description = 'Go to game start';
+    }
+
+    return (
+      <li>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    )
+  })
 
   function handlePlay(nextSquares) {
     setHistory([...history, nextSquares]);
     setXIsNext(!xIsNext);
+  }
+
+  function jumpTo(nextMove) {
+    // TODO
   }
 
   return (
@@ -16,7 +35,7 @@ export default function Game() {
         <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
       </div>
       <div className="game-info">
-        <ol>{/*TODO*/}</ol>
+        <ol>{moves}</ol>
       </div>
     </div>
   );
